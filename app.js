52ae72fd82b80a19405569c3ccba4f176f6b2d68
Jack Ojango <ojangoh2@outlook.com>
2023-07-21 10:08:25 +0300

    Update

---
+++ app.js
@@ -0,0 +1,9 @@
+// Add any custom JavaScript functionality here
+// For this example, we'll add simple alert messages for each button click
+document.getElementById("checkInBtn").addEventListener("click", function() {
+    alert("Check-in button clicked!");
+});
+
+document.getElementById("checkOutBtn").addEventListener("click", function() {
+    alert("Check-out button clicked!");
+});
