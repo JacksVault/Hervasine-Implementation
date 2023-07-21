b0376406622386c6e7cd637d67167bc8bbe969fb
Jack Ojango <ojangoh2@outlook.com>
2023-07-21 10:19:12 +0300

    Update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Add any custom JavaScript functionality here
-// For this example, we'll add simple alert messages for each button click
+//Alert messages for each button click
 document.getElementById("checkInBtn").addEventListener("click", function() {
     alert("Check-in button clicked!");
 });
@@ -7,3 +6,25 @@ document.getElementById("checkInBtn").addEventListener("click", function() {
 document.getElementById("checkOutBtn").addEventListener("click", function() {
     alert("Check-out button clicked!");
 });
+
+// Check if Geolocation is available in the user's browser
+if ("geolocation" in navigator) {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // Extract the latitude and longitude from the position object
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+  
+        // Log the latitude and longitude to the console
+        console.log("Latitude: " + latitude);
+        console.log("Longitude: " + longitude);
+      },
+      (error) => {
+        console.error("Error getting location:", error.message);
+      }
+    );
+  } else {
+    console.error("Geolocation is not available in this browser.");
+  }
+  
