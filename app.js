fa58c44c1edc8e1c24a1b06f241bf29ce2111909
Jack Ojango <ojangoh2@outlook.com>
2023-07-21 10:33:08 +0300

    Update

--- app.js
+++ app.js
@@ -11,6 +11,35 @@ document.getElementById("checkOutBtn").addEventListener("click", function() {
 let userLatitude;
 let userLongitude;
 
+// Constants for the specified region
+const regionLatitude = 35.9658064;
+const regionLongitude = -0.1673188;
+const regionRadius = 50; // In meters
+
+// Function to calculate distance between two points using the Haversine formula
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const toRadians = (value) => (value * Math.PI) / 180;
+  const earthRadius = 6371000; // Earth's radius in meters
+
+  const φ1 = toRadians(lat1);
+  const φ2 = toRadians(lat2);
+  const Δφ = toRadians(lat2 - lat1);
+  const Δλ = toRadians(lon2 - lon1);
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return earthRadius * c;
+}
+
+// Disable the "Check In" button
+function disableCheckInButton() {
+  const checkInButton = document.getElementById("checkInBtn");
+  checkInButton.disabled = true;
+}
+
 // Check if Geolocation is available in the user's browser
 if ("geolocation" in navigator) {
   // Get the user's current location
@@ -24,15 +53,29 @@ if ("geolocation" in navigator) {
       console.log("Latitude: " + userLatitude);
       console.log("Longitude: " + userLongitude);
 
-      // You can use the latitude and longitude values here for form validation or any other purpose
-      // For example, you might want to call a function to perform distance calculations or validation
-      // performFormValidation();
+      // Calculate the distance between the user's location and the specified region
+      const distance = calculateDistance(
+        userLatitude,
+        userLongitude,
+        regionLatitude,
+        regionLongitude
+      );
+
+      // Check if the user is within the specified region
+      if (distance <= regionRadius) {
+        console.log("User is within the region.");
+      } else {
+        console.log("User is out of the region.");
+      }
     },
     (error) => {
       console.error("Error getting location:", error.message);
+      // If user denies location permission or geolocation is not available, disable the "Check In" button
+      disableCheckInButton();
     }
   );
 } else {
   console.error("Geolocation is not available in this browser.");
+  // If geolocation is not available, disable the "Check In" button
+  disableCheckInButton();
 }
- 
